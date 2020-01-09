e330b3320ac8ddb8406b04595635752820f62e1d
Perry Olsson <olsson.perry@gmail.com>
2020-01-08 16:24:14 -0800

    added multiplication functionality

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-const textArea = document.querySelector("textarea");
+let a = '';
+let b;
+let eventSaver;
+let operator;
 const currentText = document.getElementById("current-text");
 
-const createSpanElement = (event) => {
+const createOperandElement = (event) => {
     let span = document.createElement("span");
+    span.setAttribute('class', 'operand selector');
     span.appendChild(document.createTextNode(event.target.value))
     currentText.appendChild(span);
 }
 
+const createAnswerElement = (answer) => {
+    let span = document.createElement('span');
+    span.setAttribute('class', 'answer selector');
+    span.appendChild(document.createTextNode(answer));
+    currentText.appendChild(span);
+}
+
+const setVar = () => {
+    let temp = document.querySelectorAll('.selector');
+    let concat = ''
+    for (item of temp) {
+        concat += item.textContent;
+    }
+    concat = parseInt(concat, 10);
+    return concat
+}
+
+const multiply = (a) => {
+    let b = setVar();
+    return a * b;
+}
+
 document.addEventListener("click", function (event) {
     if (event.target.matches(".num")) {
-        createSpanElement(event)
+        if (eventSaver === '=') {
+            currentText.textContent = ''
+            eventSaver = ''
+            a = '';
+        }
+        createOperandElement(event)
+    } else if (event.target.matches('.multiply')) {
+        if (currentText.textContent !== '') {
+            eventSaver = ''
+            operator = '*';
+            a = setVar();
+            currentText.innerHTML = ''
+        }
+    } else if (event.target.matches('.equals')) {
+        if (a !== '' && currentText.innerHTML !== '') {
+            switch (operator) {
+                case '*':
+                    b = multiply(a).toString();
+                    currentText.innerHTML = '';
+                    createAnswerElement(b);
+                    eventSaver = event.target.textContent;
+                case '+':
+                    //
+                case '-':
+                    //
+                case '/':
+                    //
+            }
+            
+        }
     }
 })
