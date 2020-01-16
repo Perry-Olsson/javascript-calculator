24c6092a7417218ff1b811e88529c310178459eb
Perry Olsson <olsson.perry@gmail.com>
2020-01-16 14:24:27 -0800

    added clear button and chain calculation functionality

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let a = ''; //store first entered number
-let b = '';
-let ans;  //store second number
-let eventSaver;
-let operator;
+let b = ''; //store second number
+let ans;  //store last answer
+let eventSaver; //Store last event when needed
+let operator; //store operator
+
+//html element where numbers entered are displayed
 const currentText = document.getElementById("current-text");
 currentText.textContent = ''
 
+//creates and displays (in currentText element) an element when a digit or decimal is pressed
 const createOperandElement = (event) => {
     let span = document.createElement("span");
     span.setAttribute('class', 'operand selector');
@@ -13,6 +16,8 @@ const createOperandElement = (event) => {
     currentText.appendChild(span);
 }
 
+//creates and displays and element for an answer to a calculation
+//different class from previous function so that backspace funciton doesn't work on it
 const createAnswerElement = (answer) => {
     let span = document.createElement('span');
     span.setAttribute('class', 'answer selector');
@@ -20,13 +25,13 @@ const createAnswerElement = (answer) => {
     currentText.appendChild(span);
 }
 
+//deletes most recently entered digit or decimal point
 const backSpace = () => {
     let temp1 = document.querySelectorAll('.operand');
-    /*let lastChildIndex = temp.length - 1;
-    temp[lastChildIndex].parentNode.removeChild(temp[lastChildIndex]);*/
     temp1[temp1.length - 1].remove();    
 }
 
+//turns entry into a number to be operated on
 const parseEntry = () => {
     let temp = document.querySelectorAll('.selector');
     let concat = ''
@@ -37,6 +42,7 @@ const parseEntry = () => {
     return concat
 }
 
+//saves first entry and/or operator when an operator key is pressed
 const setVar = (op) => {
     if (currentText.textContent !== '') {
         eventSaver = 'operator';
@@ -46,10 +52,57 @@ const setVar = (op) => {
     } else { operator = op }
 }
 
+//displays answer in currentText element
 const setAns = () => {
     currentText.textContent = '';
     createAnswerElement(ans.toString());
     eventSaver = event.target.textContent; //To clear stored variables if a number is entered directly after an answer is given
+}
+
+//evalutes given equation
+const evaluate = () => {
+    switch (operator) {
+        case 'x':
+            if (eventSaver === '=') { ans *= b; }
+            else {
+                b = parseEntry();
+                ans = a * b
+            }
+            setAns();
+            break;
+        case '+':
+            if (eventSaver === '=') { ans += b; }
+            else {
+                b = parseEntry();
+                ans = a + b;
+            }
+            setAns();
+            break;
+        case '-':
+            if (eventSaver === '=') { ans -= b; }
+            else {
+                b = parseEntry();
+                ans = a - b;
+            }
+            setAns();
+            break;//
+        case '/':
+            if (eventSaver === '=') { ans /= b; }
+            else {
+                b = parseEntry();
+                ans = a / b;
+            }
+            setAns();
+            break;//
+    }
+}
+
+//allows chain calculations e.g. 2 x 3 x 5 while displaying each answer as more 
+//numbers and operators are entered
+const chainCalculate = () => {
+    evaluate();
+    a = ans;
+    eventSaver = 'chain';
 }
 
 document.addEventListener("click", function (event) {
@@ -58,6 +111,8 @@ document.addEventListener("click", function (event) {
             currentText.textContent = ''
             eventSaver = ''
             a = '';
+        } else if (eventSaver === 'chain') {
+            currentText.textContent = '';
         }
         createOperandElement(event);
     } else if (event.target.matches('.clear-all')) {
@@ -69,49 +124,26 @@ document.addEventListener("click", function (event) {
     } else if (currentText.textContent !== '' || eventSaver === 'operator') {
         if (event.target.matches('.back-space') && eventSaver !== '=') {
             backSpace();
+        } else if (event.target.matches('.clear')) {
+            currentText.textContent = '';
         } else if (event.target.matches('.multiply')) {
-            setVar(event.target.textContent);
+            if (a !== '' && currentText.firstChild.matches('.operand')) {
+                chainCalculate();
+            } else { setVar(event.target.textContent); }
         } else if (event.target.matches('.add')) {
-            setVar(event.target.textContent);
+            if (a !== '' && currentText.firstChild.matches('.operand')) {
+                chainCalculate();
+            } else { setVar(event.target.textContent); }
         } else if (event.target.matches('.subtract')) {
-            setVar(event.target.textContent);
+            if (a !== '' && currentText.firstChild.matches('.operand')) {
+                chainCalculate();
+            } else { setVar(event.target.textContent); }
         } else if (event.target.matches('.divide')) {
-            setVar(event.target.textContent);
+            if (a !== '' && currentText.firstChild.matches('.operand')) {
+                chainCalculate();
+            } else { setVar(event.target.textContent); }
         } else if (event.target.matches('.equals') && a !== '' && currentText.textContent !== '') {
-            switch (operator) {
-                case 'x':
-                    if (eventSaver === '=') { ans *= b; }
-                    else {
-                        b = parseEntry();
-                        ans = a * b
-                    }
-                    setAns();
-                    break;
-                case '+':
-                    if (eventSaver === '=') { ans += b; }
-                    else {
-                        b = parseEntry();
-                        ans = a + b;
-                    }
-                    setAns();
-                    break;
-                case '-':
-                    if (eventSaver === '=') { ans -= b; }
-                    else {
-                        b = parseEntry();
-                        ans = a - b;
-                    }
-                    setAns();
-                    break;//
-                case '/':
-                    if (eventSaver === '=') { ans /= b; }
-                    else {
-                        b = parseEntry();
-                        ans = a / b;
-                    }
-                    setAns();
-                    break;//
-            }
+            evaluate();
         }
     }
 })
