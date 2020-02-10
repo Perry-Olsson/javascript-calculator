4174ed1f8e4fac260e9f4ddde5a313c5b67eb7b0
Perry Olsson <olsson.perry@gmail.com>
2020-02-09 16:00:25 -0800

    square root

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let operands = []; //stores operation
 let eventSaver; //Store last event when needed
 let previousAnswer;
+const currentText = document.getElementById("current-text");
+const history = document.getElementById('history');
+const textBox = document.getElementById('text-box-wrapper')
+const viewPort = document.querySelector('html');
+currentText.textContent = ''
 
 const spruceUp = () => {
     const operandMakeOver = operands.map((item, index) => {
@@ -61,13 +66,6 @@ const evaluate = () => {
     return operandArr[0];
 }
 
-//html element where numbers entered are displayed
-const currentText = document.getElementById("current-text");
-const history = document.getElementById('history');
-const textBox = document.getElementById('text-box-wrapper')
-const viewPort = document.querySelector('html');
-currentText.textContent = ''
-
 //creates and displays (in currentText element) an element when a digit or decimal is pressed
 const createOperandElement = (text) => {
     let span = document.createElement("span");
@@ -127,38 +125,46 @@ const setVar = (op) => {
     }
 }
 
-const numCompressor = () => {
+const numCompressor = (ans) => {
+    let textBoxHeight = textBox.offsetHeight;
+    let textBoxWidth = textBox.offsetWidth;
     const compress = (a, b) => {
         if (a / b < 1) {
-            return a.toFixed(2);
+            return parseFloat(a.toFixed(2));
         } else { return a.toExponential(2); }
     }
-    if (history.textContent.length >= 19) {
-        console.log('hello');
+    history.innerHTML = spruceUp(operands).join(' ');
+    if (event.target.matches('.equals')) {
+        history.innerHTML += ` = ${ans}`;
+    }
+    if (textBox.offsetHeight > textBoxHeight || textBox.offsetWidth > textBoxWidth) {
         if (operands.length > 3) {
-            if (event.target.matches('.square-root')) {
+            if (event.target.matches('.equals')) {
                 history.innerHTML = spruceUp(operands).join(' ') + ' =';
             } else {
+                let operandSaver = operands.slice(-2);
+                operands = operands.slice(0, -2);
                 operands = [evaluate()];
+                operands.push(operandSaver[0], operandSaver[1]);
                 history.innerHTML = spruceUp(operands).join(' ');
             }
         } else { history.innerHTML = spruceUp(operands).join(' ') + ' ='; }
     }
-    if (history.textContent.length >= 19) {
+    if (textBox.offsetHeight > textBoxHeight || textBox.offsetWidth > textBoxWidth) {
         if (operands.length === 1) {
             operands[0] = compress(operands[0], 1000000000);
         } else {
             if (operands[0].toString().length > operands[2].toString().length) {
                 operands[0] = compress(operands[0], 100000);
-                if (operands.join(' ').length < 19) {
-                    history.innerHTML = spruceUp(operands).join(' ') + ' =';
+                history.innerHTML = spruceUp(operands).join(' ') + ' =';
+                if (textBox.offsetHeight === textBoxHeight || textBox.offsetWidth === textBoxWidth) {
                     return
                 }
                 operands[2] = compress(operands[2], 100000);
             } else {
                 operands[2] = compress(operands[2], 100000);
-                if (operands.join(' ').length < 19) {
-                    history.innerHTML = spruceUp(operands).join(' ') + ' =';
+                history.innerHTML = spruceUp(operands).join(' ') + ' =';
+                if (textBox.offsetHeight === textBoxHeight || textBox.offsetWidth === textBoxWidth) {
                     return
                 }
                 operands[0] = compress(operands[0], 100000);
@@ -168,28 +174,28 @@ const numCompressor = () => {
     }
 }
 
-//displays answer in currentText element
-const setAns = (ans, op) => {
-    if (eventSaver !== 'chain') {
-        history.innerHTML += ` = ${ans}`;
-    }
-    numCompressor();
-    currentText.textContent = '';
+
+const answerCompressor = (ans) => {
     if (ans.toString().length > 11) {
         if (ans / 100000000 < 1) {
-            createAnswerElement(ans.toFixed(11 - (ans.toString().indexOf('.') + 1)));
+            return ans.toFixed(11 - (ans.toString().indexOf('.') + 1));
         } else {
-            createAnswerElement(ans.toExponential(2));
+            return ans.toExponential(2);
         }
     }
-    else { createAnswerElement(ans.toString()); }
+    else { return ans.toString(); }
 }
-
+//displays answer in currentText element
+const setAns = (ans, op) => {
+    numCompressor(ans);
+    currentText.textContent = '';
+    createAnswerElement(answerCompressor(ans));
+}
 
 /*allows chain calculations e.g. 2 x 3 x 5 while displaying each answer as more 
 numbers and operators are entered*/
 const chainCalculate = (op) => {
-    if (eventSaver !== '**') {
+    if (eventSaver !== '**' && eventSaver !== 'r') {
         operands.push(parseEntry());
     }
     eventSaver = 'chain';
@@ -208,9 +214,25 @@ const changeOperator = (op) => {
     history.innerHTML = spruceUp(operands).join(' ');
 }
 
+const mdas = (op) => {
+    if (currentText.textContent !== '' && operands[0] !== undefined && currentText.firstChild.matches('.operand') || eventSaver === '**' || eventSaver === 'r') {
+        chainCalculate(op);
+    } else {
+        if (eventSaver === 'chain') {
+            changeOperator(op);
+        } else {
+            setVar(op);
+        }
+    }
+}
+
+//const exponentiate = (operand, operation, event) {
+
+//}
+
 document.addEventListener("click", function (event) {
     if (event.target.matches(".num") && currentText.childElementCount < 11) {
-        if (eventSaver === '=' || eventSaver === '**') {
+        if (eventSaver === '=' || eventSaver === '**' || eventSaver === 'r') {
             currentText.textContent = '';
             history.textContent = '';
             eventSaver = '';
@@ -246,47 +268,15 @@ document.addEventListener("click", function (event) {
         } else if (currentText.textContent !== '' && event.target.matches('.clear') && currentText.firstChild.matches('.operand')) {
             currentText.textContent = '';
         } else if (event.target.matches('.multiply')) {
-            if (currentText.textContent !== '' && operands[0] !== undefined && currentText.firstChild.matches('.operand') || eventSaver === '**') {
-                chainCalculate('x');
-            } else {
-                if (eventSaver === 'chain') {
-                    changeOperator('x');
-                } else {
-                    setVar('x');
-                }
-            }
+            mdas('x');
         } else if (event.target.matches('.add')) {
-            if (currentText.textContent !== '' && operands[0] !== undefined && currentText.firstChild.matches('.operand') || eventSaver === '**') {
-                chainCalculate('+');
-            } else {
-                if (eventSaver === 'chain') {
-                    changeOperator('+');
-                } else {
-                    setVar('+');
-                }
-            }
+            mdas('+');
         } else if (event.target.matches('.subtract')) {
-            if (currentText.textContent !== '' && operands[0] !== undefined && currentText.firstChild.matches('.operand') || eventSaver === '**') {
-                chainCalculate('-');
-            } else {
-                if (eventSaver === 'chain') {
-                    changeOperator('-');
-                } else {
-                    setVar('-');
-                }
-            }
+            mdas('-');
         } else if (event.target.matches('.divide')) {
-            if (currentText.textContent !== '' && operands[0] !== undefined && currentText.firstChild.matches('.operand') || eventSaver === '**') {
-                chainCalculate('/');
-            } else {
-                if (eventSaver === 'chain') {
-                    changeOperator('/');
-                } else {
-                    setVar('/');
-                }
-            }
+            mdas('/');
         } else if (event.target.matches('.squared') && currentText.textContent !== '' && eventSaver !== 'chain') {
-            if (eventSaver === '**') {
+            if (eventSaver === '**' || eventSaver === 'r') {
                 if (operands.length < 2) {
                     operands = [];
                 }
@@ -297,21 +287,36 @@ document.addEventListener("click", function (event) {
             operands.push(`${parseEntry()}**2`);
             numCompressor();
             history.innerHTML = spruceUp(operands).join(' ');
-            if (eventSaver === 'chain' || eventSaver === 'operator' || eventSaver === '**' && operands.length > 1) {
+            if (eventSaver === 'chain' || eventSaver === 'operator' || eventSaver === '**' && operands.length > 1 || eventSaver === 'r' && operands.length > 1) {
                 let tempAns = parseEntry() ** 2;
                 currentText.textContent = '';
-                createAnswerElement(tempAns.toString());
+                createAnswerElement(answerCompressor(tempAns));
             } else { setAns(evaluate()); }
             eventSaver = '**';
         } else if (event.target.matches('.square-root') && currentText.textContent !== '' && eventSaver !== 'chain') {
+            if (eventSaver === 'r' || eventSaver === '**') {
+                if (operands.length < 2) {
+                    operands = [];
+                }
+                else {
+                    operands.pop();
+                }
+            } else if (eventSaver === '=') { operands = []; }
             operands.push(`2r${parseEntry()}`);
+            numCompressor();
             history.innerHTML = spruceUp(operands).join(' ');
-            setAns(evaluate());
-        } else if (event.target.matches('.equals') && operands != [] && currentText.textContent !== '' && eventSaver !== 'chain') {
+            if (eventSaver === 'chain' || eventSaver === 'operator' || eventSaver === '**' && operands.length > 1 || eventSaver === 'r' && operands.length > 1) {
+                let tempAns = Math.sqrt(parseEntry());
+                currentText.textContent = '';
+                createAnswerElement(answerCompressor(tempAns));
+            } else { setAns(evaluate()); }
+            eventSaver = 'r'
+        } else if (event.target.matches('.equals') && operands !== [] && currentText.textContent !== '' && eventSaver !== 'chain') {
             let temp = parseEntry();
             if (eventSaver === '=') {
                 if (operands.length < 2) {
-                    operands = [`${temp}**2`]
+                    if (operands[0].indexOf('*') !== -1) { operands = [`${temp}**2`]; }
+                    else { operands = [`2r${temp}`]; }
                 } else {
                     operands = operands.slice(-2);
                     operands.unshift(temp);
@@ -320,8 +325,11 @@ document.addEventListener("click", function (event) {
                 if (operands.length < 2) {
                     operands = [`${temp}**2`]
                 } 
+            } else if (eventSaver === 'r') {
+                if (operands.length < 2) {
+                    operands = [`2r${temp}`];
+                }
             } else { operands.push(temp); }
-            history.innerHTML = operands.join(' ');
             setAns(evaluate());
             eventSaver = '=';
         }
