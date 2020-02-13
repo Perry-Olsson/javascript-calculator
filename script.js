7cada40338ee84396f3bf6fbe2109a5f1bfe953a
Perry Olsson <olsson.perry@gmail.com>
2020-02-13 15:11:12 -0800

    all buttons working

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let operands = []; //stores operation
 let eventSaver; //Store last event when needed
-let previousAnswer;
 const currentText = document.getElementById("current-text");
 const history = document.getElementById('history');
 const textBox = document.getElementById('text-box-wrapper')
-const viewPort = document.querySelector('html');
 currentText.textContent = ''
 
 const spruceUp = () => {
@@ -21,14 +19,18 @@ const spruceUp = () => {
     return operandMakeOver;
 }
 
-const evaluate = () => {
+const evaluate = (operands) => {
     let ans;
     let operandArr = operands.map((item) => {
         if (item.length !== undefined) {
             if (item.indexOf('*') !== -1) {
                 return parseFloat(item.slice(0, item.indexOf('*')), 10) ** 2;
             } else if (item.indexOf('r') !== -1) {
-                return Math.sqrt(parseFloat(item.slice(item.indexOf('r') + 1)))
+                return Math.sqrt(parseFloat(item.slice(item.indexOf('r') + 1)));
+            } else if (item.indexOf('%') !== -1) {
+                return parseFloat(item.slice(0, -1)) / 100;
+            } else if (item === 'of') { 
+                return 'x';
             } else { return item; }
         } else { return item; }
     });
@@ -67,11 +69,12 @@ const evaluate = () => {
 }
 
 //creates and displays (in currentText element) an element when a digit or decimal is pressed
-const createOperandElement = (text) => {
+const createOperandElement = (text, sign=0) => {
     let span = document.createElement("span");
     span.setAttribute('class', 'operand selector');
     span.appendChild(document.createTextNode(text))
-    currentText.appendChild(span);
+    if (sign === 1) { currentText.prepend(span); }
+    else { currentText.appendChild(span); }
 }
 
 //creates and displays and element for an answer to a calculation
@@ -92,8 +95,8 @@ const createAnswerElement = (answer) => {
 }*/
 //deletes most recently entered digit or decimal point
 const backSpace = () => {
-    let temp1 = document.querySelectorAll('.operand');
-    temp1[temp1.length - 1].remove();    
+    let temp = document.querySelectorAll('.operand');
+    temp[temp.length - 1].remove();    
 }
 
 //turns entry into a number to be operated on
@@ -125,7 +128,13 @@ const setVar = (op) => {
     }
 }
 
-const numCompressor = (ans) => {
+const removeZeros = (ans) => {
+    let finalAnswer = ans.toFixed(11 - (ans.toString().indexOf('.') + 1)).toString();
+    while (finalAnswer[finalAnswer.length - 1] === '0') { finalAnswer = finalAnswer.slice(0, -1); }
+    return finalAnswer
+}
+
+const numCompressor = (ans, op=0) => {
     let textBoxHeight = textBox.offsetHeight;
     let textBoxWidth = textBox.offsetWidth;
     const compress = (a, b) => {
@@ -135,6 +144,9 @@ const numCompressor = (ans) => {
     }
     history.innerHTML = spruceUp(operands).join(' ');
     if (event.target.matches('.equals')) {
+        if (ans.toString().length > 11) {
+            ans = removeZeros(ans);
+        }
         history.innerHTML += ` = ${ans}`;
     }
     if (textBox.offsetHeight > textBoxHeight || textBox.offsetWidth > textBoxWidth) {
@@ -142,11 +154,14 @@ const numCompressor = (ans) => {
             if (event.target.matches('.equals')) {
                 history.innerHTML = spruceUp(operands).join(' ') + ' =';
             } else {
+                if (operands.length % 2 === 0) {
+                    operands = operands.slice(0, -1);
+                } 
                 let operandSaver = operands.slice(-2);
-                operands = operands.slice(0, -2);
-                operands = [evaluate()];
+                operands = [evaluate(operands.slice(0, -2))];
                 operands.push(operandSaver[0], operandSaver[1]);
                 history.innerHTML = spruceUp(operands).join(' ');
+                if (op !== 0) { operands.push(op); }
             }
         } else { history.innerHTML = spruceUp(operands).join(' ') + ' ='; }
     }
@@ -178,7 +193,7 @@ const numCompressor = (ans) => {
 const answerCompressor = (ans) => {
     if (ans.toString().length > 11) {
         if (ans / 100000000 < 1) {
-            return ans.toFixed(11 - (ans.toString().indexOf('.') + 1));
+            return removeZeros(ans);
         } else {
             return ans.toExponential(2);
         }
@@ -187,7 +202,7 @@ const answerCompressor = (ans) => {
 }
 //displays answer in currentText element
 const setAns = (ans, op) => {
-    numCompressor(ans);
+    numCompressor(ans, op);
     currentText.textContent = '';
     createAnswerElement(answerCompressor(ans));
 }
@@ -196,16 +211,20 @@ const setAns = (ans, op) => {
 numbers and operators are entered*/
 const chainCalculate = (op) => {
     if (eventSaver !== '**' && eventSaver !== 'r') {
-        operands.push(parseEntry());
+        if (eventSaver === '%') {
+            if (op === 'x' || op === 'of') { operands.push(`${parseEntry()}%`); }
+            else { operands.push(parseEntry() / 100); }
+        }
+        else { operands.push(parseEntry()); }
     }
+    operands.push(op);
     eventSaver = 'chain';
-    if (op === 'x' || op === '/') {
+    if (op === 'x' || op === '/' || op === 'of') {
         if (operands.indexOf('+') !== -1 || operands.indexOf('-') !== -1) {
             currentText.textContent = '';
-            numCompressor();
-        } else { setAns(evaluate()); }
-    } else { setAns(evaluate()); }
-    operands.push(op);
+            numCompressor('', op);
+        } else { setAns(evaluate(operands.slice(0, -1)), op); }
+    } else { setAns(evaluate(operands.slice(0, -1)), op); }
     history.innerHTML = spruceUp(operands).join(' ');
 }
 
@@ -220,10 +239,20 @@ const mdas = (op) => {
     } else {
         if (eventSaver === 'chain') {
             changeOperator(op);
+        } else if (eventSaver === '%') { 
+            percent(op);
         } else {
             setVar(op);
         }
     }
+}
+
+const percent = (op) => {
+    if (op === 'x' || op === 'of') { operands.push(`${parseEntry()}%`, op); }
+    else { operands.push(parseEntry() / 100, op); }
+    currentText.textContent = '';
+    history.innerHTML = spruceUp(operands).join(' ');
+    eventSaver = 'operator';
 }
 
 //const exponentiate = (operand, operation, event) {
@@ -240,6 +269,8 @@ document.addEventListener("click", function (event) {
         } else if (eventSaver === 'chain') {
             currentText.textContent = '';
             eventSaver = 'operator';
+        } else if (eventSaver === '%') {
+            percent('of');
         }
         if (event.target.textContent !== '.' && currentText.textContent === '0') {
             currentText.textContent = '';
@@ -255,11 +286,13 @@ document.addEventListener("click", function (event) {
             if (currentText.textContent.indexOf('.') === -1) {
                 createOperandElement(event.target.textContent);
             }
+        } else if (event.target.matches('.sign')) { 
+            if (currentText.textContent.indexOf('-') === -1) { createOperandElement('-', 1) }
+            else { document.querySelector('.operand').remove();}
         } else { createOperandElement(event.target.textContent); }
     } else if (event.target.matches('.clear-all')) {
         history.textContent = '';
         currentText.textContent = '';
-        a = '';
         eventSaver = '';
         operands = [];
     } else if (currentText.textContent !== '' || eventSaver === 'operator') {
@@ -267,8 +300,16 @@ document.addEventListener("click", function (event) {
             backSpace();
         } else if (currentText.textContent !== '' && event.target.matches('.clear') && currentText.firstChild.matches('.operand')) {
             currentText.textContent = '';
+        } else if (event.target.matches('.percent') && currentText.textContent.indexOf('%') === -1 && currentText.textContent !== '') {
+            if (eventSaver === '=') { 
+                history.textContent = '';
+                operands = [];
+            }
+            createOperandElement('%'); 
+            eventSaver = '%';
         } else if (event.target.matches('.multiply')) {
-            mdas('x');
+            if (eventSaver === '%') { mdas('of') }
+            else { mdas('x'); }
         } else if (event.target.matches('.add')) {
             mdas('+');
         } else if (event.target.matches('.subtract')) {
@@ -291,7 +332,7 @@ document.addEventListener("click", function (event) {
                 let tempAns = parseEntry() ** 2;
                 currentText.textContent = '';
                 createAnswerElement(answerCompressor(tempAns));
-            } else { setAns(evaluate()); }
+            } else { setAns(evaluate(operands)); }
             eventSaver = '**';
         } else if (event.target.matches('.square-root') && currentText.textContent !== '' && eventSaver !== 'chain') {
             if (eventSaver === 'r' || eventSaver === '**') {
@@ -309,13 +350,14 @@ document.addEventListener("click", function (event) {
                 let tempAns = Math.sqrt(parseEntry());
                 currentText.textContent = '';
                 createAnswerElement(answerCompressor(tempAns));
-            } else { setAns(evaluate()); }
+            } else { setAns(evaluate(operands)); }
             eventSaver = 'r'
         } else if (event.target.matches('.equals') && operands !== [] && currentText.textContent !== '' && eventSaver !== 'chain') {
             let temp = parseEntry();
             if (eventSaver === '=') {
                 if (operands.length < 2) {
                     if (operands[0].indexOf('*') !== -1) { operands = [`${temp}**2`]; }
+                    else if (operands[0].indexOf('%') !== -1) { operands = [`${temp}%`]; }
                     else { operands = [`2r${temp}`]; }
                 } else {
                     operands = operands.slice(-2);
@@ -329,8 +371,12 @@ document.addEventListener("click", function (event) {
                 if (operands.length < 2) {
                     operands = [`2r${temp}`];
                 }
+            } else if (eventSaver === '%') { 
+                if (operands.length < 2) {
+                    operands = [`${temp}%`];
+                } else { operands.push(`${temp}%`); }
             } else { operands.push(temp); }
-            setAns(evaluate());
+            setAns(evaluate(operands));
             eventSaver = '=';
         }
     }
